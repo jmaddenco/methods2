0976e1ba294ca308df7a79bd4c8fab7780061526
Joshua Madden <jmaddenco@gmail.com>
2015-09-01 10:14:23 -0600

    trying to make everything work

--- methods2.rb
+++ methods2.rb
@@ -1,14 +1,82 @@
 module Methods2
 	
 	# TODO - write elevenish?
+
+	def elevenish?(x)
+		print ("elevenish with "+ x.to_s)
+		if (x % 11 == 0) then
+			return true
+		end
+
+		if ((x + 1) % 11 == 0) then
+			puts"hello" 
+			return true
+		end
+
+		return false
+
+	end
 	
 	# TODO - write ice_cream_party?
+
+	def ice_cream_party?(ice_cream, candy)
+		if (ice_cream < 5) then 
+			return 0
+		end
+		if (candy < 5) then
+			return 0
+		end
+
+		if (ice_cream / 2 >= candy) then
+			return 2
+		end
+		if (candy / 2 >= ice_cream) then
+			return 2
+		end
+
+		if(ice_cream >=5 && candy >= 5)then
+			return 1
+		end
+
+	end
 	
 	# TODO - write successful_squirrel_party?
 	
-	# TODO - write ticket
+	def successful_squirrel_party?(nuts, weekend)
+		if(nuts >= 40 && weekend) then
+			return true
+		end
+		
 
+		if (nuts >= 40 && nuts <= 60)then
+			return true
+		end
+		return false
+
+	end
+
+
+	# TODO - write ticket
+	def ticket?(a, b, c)
+		if (a + b == 10 || a + c == 10 || b + c == 10)
+			 10# dont need then or return, || = or
+		elsif a + b - 10 == b + c || a + b - 10 == a + c
+			5
+		else
+			0
+		end
+	end
 	# TODO - write in_order?
+	def in_order?(a, b, c, b0k)
+		if a < b && c > b && !b0k
+		 	true
+		elsif c > b && b0k
+			true
+		else
+			false
+		end
+	end
+
 
 	# TODO - write less_by_ten?
 	
---
+++ methods_writer.md
@@ -0,0 +1,16 @@
+party with different/same amounts of candy
+	return the int outcome of party encoded as
+		0 = bad
+		1 = good
+		2 = great
+	party is good: 
+		ice cream and candy are both at least 5
+	party is great:
+		either ice cream or candy is at least double the amount of the other.
+	party is bad:
+		in all cases if either ice cream or candy is < 5 
+--------------------------------------------------------
+	party is successful when the # of nuts is >=40 and <=60
+	if it is the weekend the # of nuts must be >=40
+---------------------------------------------------------
+	
