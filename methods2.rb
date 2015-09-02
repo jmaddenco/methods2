c62e21ddbe4280bb42b2cf7ffb329a9f015e0662
Josh Madden <jmaddenco@gmail.com>
2015-09-02 17:33:04 -0600

    finish methods2

--- methods2.rb
+++ methods2.rb
@@ -1,188 +1,72 @@
 module Methods2
-<<<<<<< HEAD
-
-	def elevenish?(x)
-		print ("elevenish with "+ x.to_s)
-		if (x % 11 == 0) then
-			return true
-		end
-		if ((x + 1) % 11 == 0) then
-			puts"hello" 
-			return true
-		end
-		return false
-	end
-
-	def ice_cream_party?(ice_cream, candy)
-		if (ice_cream < 5) then 
-			return 0
-		end
-		if (candy < 5) then
-			return 0
-		end
-		if (ice_cream / 2 >= candy) then
-			return 2
-		end
-		if (candy / 2 >= ice_cream) then
-			return 2
-		end
-		if(ice_cream >=5 && candy >= 5)then
-			return 1
-		end
-	end
 	
-	def successful_squirrel_party?(nuts, weekend)
-		if(nuts >= 40 && weekend) then
-			return true
-		end
-		if (nuts >= 40 && nuts <= 60)then
-			return true
-		end
-		return false
-
-	end
-
-=======
-	
-	# TODO - write elevenish?
-
-	def elevenish?(x)
-		print ("elevenish with "+ x.to_s)
-		if (x % 11 == 0) then
-			return true
-		end
-
-		if ((x + 1) % 11 == 0) then
-			puts"hello" 
-			return true
-		end
-
-		return false
-
-	end
-	
-	# TODO - write ice_cream_party?
-
-	def ice_cream_party?(ice_cream, candy)
-		if (ice_cream < 5) then 
-			return 0
-		end
-		if (candy < 5) then
-			return 0
-		end
-
-		if (ice_cream / 2 >= candy) then
-			return 2
-		end
-		if (candy / 2 >= ice_cream) then
-			return 2
-		end
-
-		if(ice_cream >=5 && candy >= 5)then
-			return 1
-		end
-
-	end
-	
-	# TODO - write successful_squirrel_party?
-	
-	def successful_squirrel_party?(nuts, weekend)
-		if(nuts >= 40 && weekend) then
-			return true
-		end
-		
-
-		if (nuts >= 40 && nuts <= 60)then
-			return true
-		end
-		return false
-
-	end
-<<<<<< HEAD
-
-
-	# TODO - write ticket
-	def ticket?(a, b, c)
-		if (a + b == 10 || a + c == 10 || b + c == 10)
-			 10# dont need then or return, || = or
-		elsif a + b - 10 == b + c || a + b - 10 == a + c
-			5
+	def elevenish(num)
+		quotient, modulus = num.divmod(11)
+		if modulus == 0 || modulus == 1
+			true
 		else
-			0
-		end
-======
-
-
-	# TODO - write ticket
->>>>>>> cc1efbf278efdd06ce766f53e371fca4d115c057
-	def ticket?(a, b, c)
-		if (a + b == 10 || a + c == 10 || b + c == 10)
-			 10# dont need then or return, || = or
-		elsif a + b - 10 == b + c || a + b - 10 == a + c
-			5
-		else
-			0
-		end
-<<<<<<< HEAD
-	end
-
-	def ticket?(a, b, c)
-		if (a + b == 10 || a + c == 10 || b + c == 10)
-			 10# dont need then or return, || = or
-		elsif a + b - 10 == b + c || a + b - 10 == a + c
-			5
-		else
-			0
+			false
 		end
 	end
-
-	def in_order?(a, b, c, b0k)
-		if a < b && c > b && !b0k
-		 	true
-		elsif c > b && b0k
+	
+	def ice_cream_party(ice_cream, candy)
+		if ice_cream < 5 || candy < 5
+			0
+		elsif ice_cream >= candy*2 || candy >= ice_cream*2
+			2
+		elsif ice_cream >= 5 && candy >= 5
+			1
+		end
+	end
+	
+	def successful_squirrel_party?(weekday, num_nuts)
+		if weekday && num_nuts >= 40 && num_nuts <= 60
+			true
+		elsif !weekday && num_nuts >= 40
 			true
 		else
 			false
 		end
 	end
 
-
-=======
-
->>>>>> c366d80c29270c821d08a722923d33d9aba5a645
+	def ticket(a, b, c)
+		if a + b == 10 || a + c == 10 || b + c == 10
+			10
+		elsif a + b - 10 == b + c || a + b - 10 == a + c
+			5
+		else
+			0
+		end
 	end
-	# TODO - write in_order?
-	def in_order?(a, b, c, b0k)
-		if a < b && c > b && !b0k
-		 	true
-		elsif c > b && b0k
+
+	def in_order?(a, b, c, bOk)
+		if b > a && c > b && !bOk
+			true
+		elsif c > b && bOk
 			true
 		else
 			false
 		end
 	end
 
-
->>>>>>> cc1efbf278efdd06ce766f53e371fca4d115c057
-	# TODO - write less_by_ten?
-	def less_by_ten?
-		
+	def less_by_ten?(a, b, c)
+		if a + 10 <= b || a + 10 <= c || b + 10 <= c
+			true
+		elsif b + 10 <= a || c + 10 <= a || c + 10 <= b
+			true
+		else
+			false
+		end
 	end
-	
-	# # TODO - write fizz_string
-	# def fizz_string
-		
-	# end
-	# # TODO - write first_last_six?
-	# def first_last_six?
-		
-	# end
-	# # TODO - write rotate_left
-	# def rotate_left
-		
-	# end
-	# # TODO - write double23?
-	# def double23
-		
-	# end
+
+	def fizz_string(str)
+		if  str.start_with?("f")
+			return "Fizz"
+		elsif str.end_with?("b")
+			return "Buzz"
+		else
+			return str
+		end
+	end
+
 end
--- methods2_test.rb
+++ methods2_test.rb
@@ -8,52 +8,57 @@ class Methods2Test < MiniTest::Test
 	def setup
 		@m = Class.new do
      include Methods2
-   		end.new
-	end	
+   	end.new
+	end
 
 	def test_elevenish
-		assert_equal true, @m.elevenish?(11) 
-		assert_equal true, @m.elevenish?(22)
-		assert_equal false, @m.elevenish?(45)
-		assert_equal false, @m.elevenish?(28)
-		assert_equal true, @m.elevenish?(10)
+		assert_equal true, @m.elevenish(11)
+		assert_equal true, @m.elevenish(22)
+		assert_equal true, @m.elevenish(45)
+		
+		assert_equal false, @m.elevenish(28)
+		assert_equal false, @m.elevenish(10)
 	end
 
 	def test_ice_cream_party
-		assert_equal 0, @m.ice_cream_party?(1,1)
-		assert_equal 0, @m.ice_cream_party?(5,1)
-		assert_equal 0, @m.ice_cream_party?(2,6)
-		assert_equal 1, @m.ice_cream_party?(5,5)
-		assert_equal 1, @m.ice_cream_party?(6,5)
-		assert_equal 1, @m.ice_cream_party?(6,6)
-		assert_equal 1, @m.ice_cream_party?(5,6)
-		assert_equal 2, @m.ice_cream_party?(12,6)
-		assert_equal 2, @m.ice_cream_party?(6,12)
-		assert_equal 0, @m.ice_cream_party?(6,-12)
-	end
-
-	def test_successful_squirrel_party?
-		assert_equal true, @m.successful_squirrel_party?(55,false)
-		assert_equal false, @m.successful_squirrel_party?(77,false)
-		assert_equal false, @m.successful_squirrel_party?(20,false)
-		assert_equal true, @m.successful_squirrel_party?(40,false)
-		assert_equal true, @m.successful_squirrel_party?(60,false)
-		assert_equal true, @m.successful_squirrel_party?(65,true)
-<<<<<<< HEAD
-=======
-#<<<<<<< HEAD
->>>>>>> cc1efbf278efdd06ce766f53e371fca4d115c057
-	end
-
-	def test_ticket?
-		assert_equal 10, @m.ticket?(1, 9, 19)
-		assert_equal 10, @m.ticket?(3, 1, 7)
-		assert_equal 10, @m.ticket?(3, 5, 5)
+		assert_equal 0, @m.ice_cream_party(0, 0)
+		assert_equal 0, @m.ice_cream_party(6, 2)
+		assert_equal 0, @m.ice_cream_party(3, 10)
 		
-		assert_equal 5, @m.ticket?(13, 5, 3)
-		assert_equal 5, @m.ticket?(4, 11, 1)
+		assert_equal 1, @m.ice_cream_party(5, 5)
+		assert_equal 1, @m.ice_cream_party(6, 7)
+		assert_equal 1, @m.ice_cream_party(10, 6)
+		
+		assert_equal 2, @m.ice_cream_party(5, 10)
+		assert_equal 2, @m.ice_cream_party(12, 6)
+		assert_equal 2, @m.ice_cream_party(30, 60)
+	end
 
-		assert_equal 0, @m.ticket?(4, 1, 1)
+	def test_successful_squirrel_party
+		assert_equal true, @m.successful_squirrel_party?(true, 40) 	
+		assert_equal true, @m.successful_squirrel_party?(true, 60) 	
+		assert_equal true, @m.successful_squirrel_party?(true, 50) 	
+
+		assert_equal true, @m.successful_squirrel_party?(false, 40) 	
+		assert_equal true, @m.successful_squirrel_party?(false, 60) 	
+		assert_equal true, @m.successful_squirrel_party?(false, 50) 	
+		assert_equal true, @m.successful_squirrel_party?(false, 90) 	
+
+		assert_equal false, @m.successful_squirrel_party?(true, 20) 	
+		assert_equal false, @m.successful_squirrel_party?(true, 70) 	
+
+		assert_equal false, @m.successful_squirrel_party?(false, 30) 	
+	end
+
+	def test_ticket
+		assert_equal 10, @m.ticket(1, 9, 19)
+		assert_equal 10, @m.ticket(3, 1, 7)
+		assert_equal 10, @m.ticket(3, 5, 5)
+		
+		assert_equal 5, @m.ticket(13, 5, 3)
+		assert_equal 5, @m.ticket(4, 11, 1)
+
+		assert_equal 0, @m.ticket(4, 1, 1)
 	end
 
 	def test_in_order
@@ -61,37 +66,23 @@ class Methods2Test < MiniTest::Test
 		assert_equal true, @m.in_order?(1, 2, 7, false)
 		assert_equal true, @m.in_order?(6, 1, 7, true)
 
-		assert_equal false, @m.in_order?(6, 8, 2, true)
-		assert_equal false, @m.in_order?(7, 5, 1, false)
-	end
-<<<<<<< HEAD
-=======
-#=======
-	end
->>>>>>> cc1efbf278efdd06ce766f53e371fca4d115c057
-
-	def test_ticket?
-		assert_equal 10, @m.ticket?(1, 9, 19)
-		assert_equal 10, @m.ticket?(3, 1, 7)
-		assert_equal 10, @m.ticket?(3, 5, 5)
-		
-		assert_equal 5, @m.ticket?(13, 5, 3)
-		assert_equal 5, @m.ticket?(4, 11, 1)
-
-		assert_equal 0, @m.ticket?(4, 1, 1)
-<<<<<<< HEAD
+		assert_equal false, @m.in_order?(6, 8, 2, false)
+		assert_equal false, @m.in_order?(7, 5, 1, true)
 	end
 
-	def less_by_ten
-		assert_equal true, @m.less_by_ten(1, 10, 12)
+	def test_less_by_ten
+		assert_equal true, @m.less_by_ten?(1, 11, 5)
+		assert_equal true, @m.less_by_ten?(8, 16, 6)
+		assert_equal true, @m.less_by_ten?(10, 11, 20)
+
+		assert_equal false, @m.less_by_ten?(1, 1, 2)
+		assert_equal false, @m.less_by_ten?(14, 11, 20)
+		assert_equal false, @m.less_by_ten?(18, 15, 23)
 	end
 
-=======
+	def test_fizz_string
+		assert_equal "Fizz", @m.fizz_string("front")
+		assert_equal "Buzz", @m.fizz_string("bub")
 	end
 
-#>>>>>>> c366d80c29270c821d08a722923d33d9aba5a645
-	# def less_by_ten
-		
-	# end
->>>>>>> cc1efbf278efdd06ce766f53e371fca4d115c057
 end
