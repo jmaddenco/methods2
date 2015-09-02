893b67d1486096425210a5d2fb5f162607b40b8f
Josh Madden <jmaddenco@gmail.com>
2015-09-02 00:02:01 -0600

    all

--- methods2.rb
+++ methods2.rb
@@ -1,39 +1,96 @@
 module Methods2
-	
-	# TODO - write elevenish?
 
 	def elevenish?(x)
-		
+		print ("elevenish with "+ x.to_s)
 		if (x % 11 == 0) then
-			return x
-
-			elsif ((x + 1) % 11 == 0) then
-				return false
-		end
-
-	end
-	
-	# TODO - write ice_cream_party?
-
-	def ice_cream_party?(y)
-		if(y = 2) then
 			return true
+		end
+		if ((x + 1) % 11 == 0) then
+			puts"hello" 
+			return true
+		end
+		return false
+	end
+
+	def ice_cream_party?(ice_cream, candy)
+		if (ice_cream < 5) then 
+			return 0
+		end
+		if (candy < 5) then
+			return 0
+		end
+		if (ice_cream / 2 >= candy) then
+			return 2
+		end
+		if (candy / 2 >= ice_cream) then
+			return 2
+		end
+		if(ice_cream >=5 && candy >= 5)then
+			return 1
+		end
 	end
 	
-	# TODO - write successful_squirrel_party?
-	
-	# TODO - write ticket
+	def successful_squirrel_party?(nuts, weekend)
+		if(nuts >= 40 && weekend) then
+			return true
+		end
+		if (nuts >= 40 && nuts <= 60)then
+			return true
+		end
+		return false
 
-	# TODO - write in_order?
+	end
+
+	def ticket?(a, b, c)
+		if (a + b == 10 || a + c == 10 || b + c == 10)
+			 10# dont need then or return, || = or
+		elsif a + b - 10 == b + c || a + b - 10 == a + c
+			5
+		else
+			0
+		end
+	end
+
+	def ticket?(a, b, c)
+		if (a + b == 10 || a + c == 10 || b + c == 10)
+			 10# dont need then or return, || = or
+		elsif a + b - 10 == b + c || a + b - 10 == a + c
+			5
+		else
+			0
+		end
+	end
+
+	def in_order?(a, b, c, b0k)
+		if a < b && c > b && !b0k
+		 	true
+		elsif c > b && b0k
+			true
+		else
+			false
+		end
+	end
+
 
 	# TODO - write less_by_ten?
+	def less_by_ten?
+		
+	end
 	
-	# TODO - write fizz_string
-
-	# TODO - write first_last_six?
-
-	# TODO - write rotate_left
-
-	# TODO - write double23?
-
+	# # TODO - write fizz_string
+	# def fizz_string
+		
+	# end
+	# # TODO - write first_last_six?
+	# def first_last_six?
+		
+	# end
+	# # TODO - write rotate_left
+	# def rotate_left
+		
+	# end
+	# # TODO - write double23?
+	# def double23
+		
+	# end
 end
--- methods2_test.rb
+++ methods2_test.rb
@@ -8,19 +8,72 @@ class Methods2Test < MiniTest::Test
 	def setup
 		@m = Class.new do
      include Methods2
-   	end.new
-	end
+   		end.new
+	end	
 
 	def test_elevenish
 		assert_equal true, @m.elevenish?(11) 
 		assert_equal true, @m.elevenish?(22)
-		assert_equal true, @m.elevenish?(45)
+		assert_equal false, @m.elevenish?(45)
 		assert_equal false, @m.elevenish?(28)
-		assert_equal false, @m.elevenish?(10)
+		assert_equal true, @m.elevenish?(10)
 	end
 
-	def ice_cream_party
-		assert_equal true, @m.ice_cream_party?(2)
+	def test_ice_cream_party
+		assert_equal 0, @m.ice_cream_party?(1,1)
+		assert_equal 0, @m.ice_cream_party?(5,1)
+		assert_equal 0, @m.ice_cream_party?(2,6)
+		assert_equal 1, @m.ice_cream_party?(5,5)
+		assert_equal 1, @m.ice_cream_party?(6,5)
+		assert_equal 1, @m.ice_cream_party?(6,6)
+		assert_equal 1, @m.ice_cream_party?(5,6)
+		assert_equal 2, @m.ice_cream_party?(12,6)
+		assert_equal 2, @m.ice_cream_party?(6,12)
+		assert_equal 0, @m.ice_cream_party?(6,-12)
+	end
+
+	def test_successful_squirrel_party?
+		assert_equal true, @m.successful_squirrel_party?(55,false)
+		assert_equal false, @m.successful_squirrel_party?(77,false)
+		assert_equal false, @m.successful_squirrel_party?(20,false)
+		assert_equal true, @m.successful_squirrel_party?(40,false)
+		assert_equal true, @m.successful_squirrel_party?(60,false)
+		assert_equal true, @m.successful_squirrel_party?(65,true)
+	end
+
+	def test_ticket?
+		assert_equal 10, @m.ticket?(1, 9, 19)
+		assert_equal 10, @m.ticket?(3, 1, 7)
+		assert_equal 10, @m.ticket?(3, 5, 5)
+		
+		assert_equal 5, @m.ticket?(13, 5, 3)
+		assert_equal 5, @m.ticket?(4, 11, 1)
+
+		assert_equal 0, @m.ticket?(4, 1, 1)
+	end
+
+	def test_in_order
+		assert_equal true, @m.in_order?(1, 2, 7, true)
+		assert_equal true, @m.in_order?(1, 2, 7, false)
+		assert_equal true, @m.in_order?(6, 1, 7, true)
+
+		assert_equal false, @m.in_order?(6, 8, 2, true)
+		assert_equal false, @m.in_order?(7, 5, 1, false)
+	end
+
+	def test_ticket?
+		assert_equal 10, @m.ticket?(1, 9, 19)
+		assert_equal 10, @m.ticket?(3, 1, 7)
+		assert_equal 10, @m.ticket?(3, 5, 5)
+		
+		assert_equal 5, @m.ticket?(13, 5, 3)
+		assert_equal 5, @m.ticket?(4, 11, 1)
+
+		assert_equal 0, @m.ticket?(4, 1, 1)
+	end
+
+	def less_by_ten
+		assert_equal true, @m.less_by_ten(1, 10, 12)
 	end
 
 end
